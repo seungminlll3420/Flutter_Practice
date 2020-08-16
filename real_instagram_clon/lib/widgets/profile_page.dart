147b9d850265060a51a9d4731f6dff9d9f819932
✔   seungmin <45160518+seungminlll3420@users.noreply.github.com>
2020-08-16 23:11:33 +0900

    add CustomScrollView and SliverChildListDelegate
    스크롤 가능한 다양한(List, grid) 커스텀 뷰

--- real_instagram_clon/lib/widgets/profile_page.dart
+++ real_instagram_clon/lib/widgets/profile_page.dart
@@ -1,4 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:real_instagram_clon/utils/profile_img_path.dart';
+import 'package:real_instagram_clon/utils/size.dart';
+import 'dart:ui';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -18,19 +22,12 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Stack(
         children: [_sideMenu(), _profile()],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _menuOpend = !_menuOpend;
-          });
-        },
-      ),
     );
   }
 
   Widget _sideMenu() {
     return AnimatedContainer(
-      color: Colors.yellow,
+      color: Colors.grey[200],
       curve: Curves.easeInOut,
       duration: Duration(milliseconds: duration),
       transform: Matrix4.translationValues(
@@ -38,13 +35,31 @@ class _ProfilePageState extends State<ProfilePage> {
         0,
         0,
       ),
+      child: SafeArea(
+        child: SizedBox(
+          width: menuWidth,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: common_gap, vertical: common_gap),
+                child: FlatButton(
+                  child: Text('dwd'),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   Widget _profile() {
     return AnimatedContainer(
       curve: Curves.easeInOut,
-      color: Colors.green,
+      color: Colors.transparent,
       duration: Duration(
         milliseconds: duration,
       ),
@@ -53,6 +68,109 @@ class _ProfilePageState extends State<ProfilePage> {
         0,
         0,
       ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            _usernameIconButton(),
+            Expanded(
+              child: CustomScrollView(
+                slivers: [
+                  SliverList(
+                    delegate: SliverChildListDelegate([
+                      _getProfileHeader,
+                      Padding(
+                        padding: const EdgeInsets.only(left: common_gap),
+                        child: Text('username',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.symmetric(horizontal: common_gap),
+                        child: Text('dwdlefkefsldfoiefkdmpfiekfa ',
+                            style: TextStyle(fontWeight: FontWeight.w200)),
+                      ),
+                    ]),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row get _getProfileHeader => Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(common_xs_gap),
+            child: CircleAvatar(
+              radius: 40.0,
+              backgroundImage:
+                  CachedNetworkImageProvider(getProfileImagPath("username")),
+            ),
+          ),
+          Expanded(
+            child: Table(
+              children: [
+                TableRow(children: [
+                  _getStatusValueWidget('1'),
+                  _getStatusValueWidget('2'),
+                  _getStatusValueWidget('3'),
+                ]),
+                TableRow(children: [
+                  _getStatusLabelWidget('Posts'),
+                  _getStatusLabelWidget('Followers'),
+                  _getStatusLabelWidget('Following'),
+                ])
+              ],
+            ),
+          )
+        ],
+      );
+  Widget _getStatusValueWidget(String val) => Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: common_s_gap),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(val,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+        ),
+      );
+  Widget _getStatusLabelWidget(String val) => Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: common_s_gap),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(val,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w400)),
+          ),
+        ),
+      );
+
+  Row _usernameIconButton() {
+    return Row(
+      children: [
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.only(left: common_gap),
+          child: Text(
+            's_omn._',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        )),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _menuOpend = !_menuOpend;
+            });
+          },
+          icon: Icon(Icons.menu),
+        )
+      ],
     );
   }
 }
